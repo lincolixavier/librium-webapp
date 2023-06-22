f22735120ee3ea6e2d93252d75e8512583d1bbfc
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-06-21 22:14:33 -0300

    feat: add typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,35 @@
 module.exports = {
+  root: true,
   env: { node: true },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   overrides: [
     // Configuration for Vue/Javascript files
     {
-      files: ['**/*.vue', '**/*.js'],
+      files: ['**/*.vue', '**/*.ts'],
       plugins: ['unused-imports', 'simple-import-sort', 'jsx-a11y'],
-      extends: ['plugin:prettier/recommended', 'plugin:jsx-a11y/recommended'],
+      extends: [
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting'
+      ],
       env: {
-        browser: true,
+        browser: true
       },
       rules: {
         'prettier/prettier': ['error', { singleQuote: true }],
-        'no-restricted-syntax': [
-          'error',
-          'ForInStatement',
-          'LabeledStatement',
-          'WithStatement',
-        ],
+        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
         'unused-imports/no-unused-imports': 'error',
-        'unused-imports/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      },
+        'unused-imports/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+      }
     },
     // Configuration for testing with vitest
     {
       files: ['**/*.test.js', '**/*.test.jsx'],
       plugins: ['vitest'],
-      extends: ['plugin:vitest/recommended'],
-    },
-    // Configuration for testing with cypress
-    {
-      files: ['**/*.cy.js'],
-      plugins: ['cypress'],
-      extends: ['plugin:cypress/recommended'],
-      env: {
-        'cypress/globals': true,
-      },
-    },
-  ],
-};
+      extends: ['plugin:vitest/recommended']
+    }
+  ]
+}
